cc12adf6abb948a929461b7133ff1763d201e75d
TolgaGubul <gubultolga@hotmail.com>
2023-06-07 17:01:43 +0300

    tolga controller eklendi.

---
+++ Armut.WebApp/Controllers/Tolga.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Armut.WebApp.Controllers
+{
+    public class Tolga : Controller
+    {
+        [Route("tolga")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
