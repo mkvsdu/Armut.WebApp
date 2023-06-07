e012fa5d0ad1fdf402226d7e270df0ba18188ab3
Mervenur Öztürk <mervenurozturk1999@hotmail.com>
2023-06-07 16:48:28 +0300

    mervecontroller route kısmı silindi

--- Armut.WebApp/Controllers/MerveController.cs
+++ Armut.WebApp/Controllers/MerveController.cs
@@ -4,7 +4,7 @@ namespace Armut.WebApp.Controllers
 {
     public class MerveController : Controller
     {
-        [Route("merve")]
+
         public IActionResult Index()
         {
             return View();
