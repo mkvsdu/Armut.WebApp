18c1fb20536962e610fbf1df9bc02230941df8f9
mehmetayilmaz <77573251+mehmetayilmaz@users.noreply.github.com>
2023-06-07 16:39:39 +0300

    mehmetcontroller ve ındex eklendi

---
+++ Armut.WebApp/Controllers/MehmetController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Armut.WebApp.Controllers
+{
+    public class MehmetController : Controller
+    {
+
+        [Route("Mehmet")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
