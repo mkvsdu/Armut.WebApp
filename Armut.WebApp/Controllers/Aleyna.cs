d1667a6003aed1a3158414c1ab6571dfcd55fe5f
Aleyna Çakır <129675280+aleynacakirr@users.noreply.github.com>
2023-06-07 16:41:05 +0300

    aleyna controller/view oluşturuldu.

---
+++ Armut.WebApp/Controllers/Aleyna.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Armut.WebApp.Controllers
+{
+    public class Aleyna : Controller
+    {
+        [Route("aleyna")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
