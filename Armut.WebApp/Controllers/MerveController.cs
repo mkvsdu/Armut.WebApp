70e47de67f111bf5d3120daea7fdf6308857220a
Mervenur Öztürk <mervenurozturk1999@hotmail.com>
2023-06-07 16:39:08 +0300

    mervecontroller/view oluşturuldu

---
+++ Armut.WebApp/Controllers/MerveController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Armut.WebApp.Controllers
+{
+    public class MerveController : Controller
+    {
+        [Route("merve")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
