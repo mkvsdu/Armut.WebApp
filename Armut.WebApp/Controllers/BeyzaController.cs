9b7c16dee83095437f0baa6bca57fae4386ab32b
Beyzanurcelikk <132361660+Beyzanurcelikk@users.noreply.github.com>
2023-06-07 16:39:31 +0300

    Beyza adına controller ve view oluşturdum.

---
+++ Armut.WebApp/Controllers/BeyzaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Armut.WebApp.Controllers
+{
+    public class BeyzaController : Controller
+    {
+        [Route("beyza")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
